8fb2071eb6124bb74e91dedd0bbb18f7e67adad1
catherine-morse <cmorse@udel.edu>
2016-11-28 19:40:40 -0500

    Finished educational tab. Implemented maps tab.
    Maps tab has clickability and can read/write to database.

---
+++ Resource.java
@@ -0,0 +1,47 @@
+package edu.udel.cisc475.team1.greentech_android;
+
+import java.util.UUID;
+
+/**
+ * Created by writingcenter on 11/6/16.
+ */
+
+public class Resource {
+    private String title;
+    private String website;
+    private String description;
+
+    public Resource() {
+
+    }
+
+    public Resource(String title, String website, String description) {
+        this.title = title;
+        this.website = website;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ SearchFragment.java
@@ -0,0 +1,16 @@
+package edu.udel.cisc475.team1.greentech_android;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
